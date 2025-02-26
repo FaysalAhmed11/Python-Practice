68972147257dfafb8fb55ab398888faecae276cc
Md. Faysal Ahmed <falampur@gmail.com>
2025-02-25 21:04:01 -0330

    update the code

--- Audio Extractor/audio_extractor.py
+++ Audio Extractor/audio_extractor.py
@@ -1,1 +1,9 @@
-import unmpy as np
+#using moviepy - pip install moviepy
+
+import moviepy.editor
+
+
+cvt_video = moviepy.editor.VideoFileClip("nature.mp4")
+ext_audio = cvt_video.audio
+
+ext_audio.write.audiofile("audio_extracted.mp3")
