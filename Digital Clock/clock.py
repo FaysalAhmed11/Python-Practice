aa8f81aae9dae5bac84623a652c963869439edbe
Md. Faysal Ahmed <falampur@gmail.com>
2025-02-28 19:42:28 -0330

    Digital clock using python

---
+++ Digital Clock/clock.py
@@ -0,0 +1,19 @@
+from tkinter import Tk
+from tkinter import Label
+import time
+
+root = Tk()
+root.title("Python Clock")
+
+def current_time():
+    display_time = time.strftime("%H:%M:%S %p")
+    digital_clock.config(text=display_time)
+    digital_clock.after(200, current_time)
+
+
+digital_clock = Label(root, font=('Times New Roman', 120), bg='green', fg='red')
+digital_clock.pack()
+
+current_time()
+
+root.mainloop()
