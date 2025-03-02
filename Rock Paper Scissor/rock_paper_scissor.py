2bf7aa13936c8889f421c87ef4a201b948b5a852
Md. Faysal Ahmed <falampur@gmail.com>
2025-03-01 22:50:03 -0330

    The famous game rock paper scissir creating

---
+++ Rock Paper Scissor/rock_paper_scissor.py
@@ -0,0 +1,13 @@
+# Import Required Library
+from tkinter import *
+import random
+
+# Create Object
+window = Tk()
+window.title("Rock Paper Scissor")
+window.configure(background="black")
+
+
+
+# Execute Tkinter
+window.mainloop()
